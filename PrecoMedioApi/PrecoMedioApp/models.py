fb0ea32f13f53e73e4e972465a8a5238dccb7e55
lucasribeirojorge <lucccas.jorge@gmail.com>
2024-04-08 12:12:57 -0300

    alterado campo dateofsearch para datetime para pegar o horario da consulta

--- PrecoMedioApi/PrecoMedioApp/models.py
+++ PrecoMedioApi/PrecoMedioApp/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 class Products(models.Model):
     ProductId = models.AutoField(primary_key=True)
-    Model = models.CharField(max_length=100)  # Alterei para CharField
-    StorageGB = models.IntegerField()  # Alterei para IntegerField
-    Brand = models.CharField(max_length=100)  # Alterei para CharField
+    Model = models.CharField(max_length=100)  
+    StorageGB = models.IntegerField()  
+    Brand = models.CharField(max_length=100)  
 
 class PriceTracker(models.Model):
-    Model = models.CharField(max_length=100)  # Alterei para CharField
-    DateOfSearch = models.DateField()
+    Model = models.CharField(max_length=100) 
+    DateOfSearch = models.DateTimeField()
     Price = models.DecimalField(max_digits=10, decimal_places=2)
-    SearchString = models.CharField(max_length=100)  # Alterei para CharField
-    Product = models.ForeignKey(Products, on_delete=models.CASCADE)  # Alterei para ForeignKey
+    SearchString = models.CharField(max_length=100)  
+    Product = models.ForeignKey(Products, on_delete=models.CASCADE) 
     Supplier = models.CharField(max_length=100)
