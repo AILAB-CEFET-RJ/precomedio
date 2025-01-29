bd6ad5ccec331ec74835c471cc69658bb9631df8
dayv1son <dayvisonaoc@gmail.com>
2025-01-28 23:52:33 -0300

    Desconsidera produtos estrangeiros + filtra apenas pelo produto da pesquisa

--- backend/PrecoMedioApp/utils.py
+++ backend/PrecoMedioApp/utils.py
@@ -29,62 +29,65 @@ def extrair_resultados(soup):
     return soup_ads, soup_results
 
 
-
 def obter_modelos_e_precos(soup_results, soup_ads, model):
     palavras_proibidas = ["vitrine", "usado", "recondicionado", "como novo", "zerado", "seminovo", "semi novo", "renovada", "renovado"]
     list_priceTracker = []
 
     for result in soup_results:
         title = result.find(re.compile('^h\d$')).get_text()
-        if not any(proibida in title.lower() for proibida in palavras_proibidas):
-            storage_match = re.search(r'\d+GB', title)
-            if storage_match:
-                storage_size = storage_match.group()
-                product = Products(Model=title,
-                                   StorageGB=int(storage_size[:-2]),
-                                   Brand=get_brand(title))
-                price_element = result.find("span", {"class": "a8Pemb OFFNJ"})
-                supplier_span = result.find("div", {"class": "aULzUe IuHnof"})
-                supplier = supplier_span.get_text(strip=True) if supplier_span else None
-                if price_element and not result.find("span", {"class": "tD1ls"}):  
-                    price_text = price_element.get_text()
-                    price = obter_preco(price_text)
-                    if price is not None:
-                        if not has_similar_product(title, model):
-                            unidade_priceTracker = PriceTracker(Model=title,
-                                                                DateOfSearch=timezone.now(),
-                                                                Price = price,
-                                                                SearchString=model,
-                                                                Product=product,
-                                                                Supplier=supplier)
-                            
-                            list_priceTracker.append(unidade_priceTracker)
+
+        if contains_product(title, model):
+            if not any(proibida in title.lower() for proibida in palavras_proibidas):
+                storage_match = re.search(r'\d+GB', title)
+                if storage_match:
+                    storage_size = storage_match.group()
+                    product = Products(Model=title,
+                                       StorageGB=int(storage_size[:-2]),
+                                       Brand=get_brand(title))
+                    price_element = result.find("span", {"class": "a8Pemb OFFNJ"})
+                    supplier_span = result.find("div", {"class": "aULzUe IuHnof"})
+                    supplier = supplier_span.get_text(strip=True) if supplier_span else None
+                    if price_element and not result.find("span", {"class": "tD1ls"}) and not result.find("span", {"class": "hahPbb"}):  
+                        price_text = price_element.get_text()
+                        price = obter_preco(price_text)
+                        if price is not None:
+                            if not has_similar_product(title, model):
+                                unidade_priceTracker = PriceTracker(Model=title,
+                                                                    DateOfSearch=timezone.now(),
+                                                                    Price = price,
+                                                                    SearchString=model,
+                                                                    Product=product,
+                                                                    Supplier=supplier)
+
+                                list_priceTracker.append(unidade_priceTracker)
 
     for ad in soup_ads:
         title = ad.find(re.compile('^h\d$')).get_text()
-        if not any(proibida in title.lower() for proibida in palavras_proibidas):
-            storage_match = re.search(r'\d+GB', title)
-            if storage_match:
-                storage_size = storage_match.group()
-                product = Products(Model=title,
-                                   StorageGB=int(storage_size[:-2]),
-                                   Brand=get_brand(title))
-                price_element = ad.find("span", {"class": "T14wmb"})
-                supplier_div = ad.find("div", {"class": "sh-np__seller-container"})
-                supplier = supplier_div.get_text(strip=True) if supplier_div else None
-                if price_element and not ad.find("span", {"class": "tD1ls"}):
-                    price_text = price_element.get_text()
-                    price = obter_preco(price_text)
-                    if price is not None:
-                        if not has_similar_product(title, model):
-                            unidade_priceTracker = PriceTracker(Model=title,
-                                                                DateOfSearch=timezone.now(),
-                                                                Price = price,
-                                                                SearchString=model,
-                                                                Product=product,
-                                                                Supplier=supplier)
-                            
-                            list_priceTracker.append(unidade_priceTracker)
+
+        if contains_product(title, model):
+            if not any(proibida in title.lower() for proibida in palavras_proibidas):
+                storage_match = re.search(r'\d+GB', title)
+                if storage_match:
+                    storage_size = storage_match.group()
+                    product = Products(Model=title,
+                                       StorageGB=int(storage_size[:-2]),
+                                       Brand=get_brand(title))
+                    price_element = ad.find("span", {"class": "T14wmb"})
+                    supplier_div = ad.find("div", {"class": "sh-np__seller-container"})
+                    supplier = supplier_div.get_text(strip=True) if supplier_div else None
+                    if price_element and not ad.find("span", {"class": "tD1ls"}) and not ad.find("span", {"class": "hahPbb"}):
+                        price_text = price_element.get_text()
+                        price = obter_preco(price_text)
+                        if price is not None:
+                            if not has_similar_product(title, model):
+                                unidade_priceTracker = PriceTracker(Model=title,
+                                                                    DateOfSearch=timezone.now(),
+                                                                    Price = price,
+                                                                    SearchString=model,
+                                                                    Product=product,
+                                                                    Supplier=supplier)
+                                
+                                list_priceTracker.append(unidade_priceTracker)
     return list_priceTracker
 
 def has_similar_product(title, model):
@@ -93,3 +96,22 @@ def has_similar_product(title, model):
         if similar(title, product.Model) > 1: 
             return True
     return False
+
+def contains_product(title, model):
+    title = title.lower()
+    model = model.lower()
+    
+    title = re.sub(r'(\d+)([a-zA-Z]+)', r'\1 \2', title)
+    tokens = re.findall(r'[a-zA-Z]+|\d+', model)
+    
+    termos_pesquisa = []
+    i = 0
+    while i < len(tokens):
+        if i < len(tokens) - 1 and tokens[i].isalpha() and tokens[i + 1].isdigit():
+            termos_pesquisa.append(f"{tokens[i]} {tokens[i + 1]}")
+            i += 2  
+        else:
+            termos_pesquisa.append(tokens[i])
+            i += 1
+    
+    return all(termo in title for termo in termos_pesquisa)
