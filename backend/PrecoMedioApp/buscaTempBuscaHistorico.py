faf125282cc0da238e5c90fbb34b29cc671621dc
Gabriel do Nascimento Alves <78627282+ElCid220@users.noreply.github.com>
2025-02-01 12:36:38 -0300

    Update buscaTempBuscaHistorico.py

--- backend/PrecoMedioApp/buscaTempBuscaHistorico.py
+++ backend/PrecoMedioApp/buscaTempBuscaHistorico.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.get('http://127.0.0.1:8000/reset_and_query/')
+response = requests.get('http://127.0.0.1:8000/buscaDiaria_alimentarConsolidada/')
 print(response.text)
