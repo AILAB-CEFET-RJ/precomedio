88b1091c28c306a3ea1b93734a5378d231fed1b1
mtsfrancisco <matheusdabmlj@gmail.com>
2025-04-13 22:06:45 -0300

    visualizaçao django admin

--- backend/PrecoMedioApp/admin.py
+++ backend/PrecoMedioApp/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Products, PriceTracker, Busca_consolidado, Favorites
 
-admin.site.register(Products)
-admin.site.register(PriceTracker)
-admin.site.register(Busca_consolidado)
-admin.site.register(Favorites)
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('ProductId', 'Model', 'StorageGB', 'Brand')
+
+@admin.register(PriceTracker)
+class PriceTrackerAdmin(admin.ModelAdmin):
+    list_display = ('Model', 'DateOfSearch', 'Price', 'Supplier', 'Product')
+
+@admin.register(Busca_consolidado)
+class BuscaConsolidadoAdmin(admin.ModelAdmin):
+    list_display = ('Consolidadoid', 'SearchString', 'AvgPrice', 'MinPrice', 'DateOfSearch')
+
+@admin.register(Favorites)
+class FavoritesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'price_tracker', 'date_added')
