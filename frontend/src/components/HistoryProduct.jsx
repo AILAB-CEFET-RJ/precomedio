85b4dd1936588a48a5df002d04352ee04f3d1477
Danilo011 <danilofreire.h@gmail.com>
2025-04-28 16:40:39 -0300

    Grafico
    Gráfico de preço x tempo

--- frontend/src/components/HistoryProduct.jsx
+++ frontend/src/components/HistoryProduct.jsx
@@ -1,16 +1,40 @@
 import { useEffect, useState } from "react";
 import { GetLowerPriceHistory } from "../requests/requestsLowerPriceHistory";
 import { GetAllMeanPricesLast30Days } from "../requests/requestsThirtyDaysAverage"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import axios from 'axios';
 import Header from "./Header";
 import Footer from "./Footer";
-// Vai funcionar como uma dashboard com várias tabelas e gráficos.
+
 const HistoryProduct = () => {
   const [lowerPriceHistory, setLowerPriceHistory] = useState([]);
   const [averagePrices, setAveragePrices] = useState({});
   const [load, setLoad] = useState(false);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     loadPriceHistory();
+    axios.get('http://127.0.0.1:8000/mean-prices/last-6-months/')
+      .then(response => {
+        const products = response.data;
+
+        const formattedData = Object.keys(products).map(productName => {
+          const prices = products[productName];
+          const monthlyData = Object.keys(prices).map(month => ({
+            month,
+            product: productName,
+            price: prices[month]
+          }));
+          return monthlyData;
+        });
+
+        const flattenedData = formattedData.flat();
+
+        setData(flattenedData);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar dados:', error);
+      });
   }, []);
 
   const loadPriceHistory = async () => {
@@ -18,7 +42,7 @@ const HistoryProduct = () => {
     try {
       const response = await GetLowerPriceHistory();
       const allMeanPrices = await GetAllMeanPricesLast30Days();
-  
+
       if (response && Array.isArray(response)) {
         const lowerPrices = response;
         setLowerPriceHistory(lowerPrices);
@@ -26,7 +50,7 @@ const HistoryProduct = () => {
         allMeanPrices.forEach((item) => {
           averagePriceMap[item.SearchString] = item.MeanPriceLast30Days;
         });
-  
+
         setAveragePrices(averagePriceMap);
       } else {
         setLowerPriceHistory([]);
@@ -82,6 +106,16 @@ const HistoryProduct = () => {
                   </tbody>
                 </table>
               </div>
+              <ResponsiveContainer width="100%" height={400}>
+                <LineChart data={data}
+                  margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="month" />
+                  <YAxis domain={['auto', 'auto']} />
+                  <Tooltip />
+                  <Line type="stepBefore" dataKey="price" stroke="#008000" activeDot={{ r: 8 }} />
+                </LineChart>
+              </ResponsiveContainer>
             </article>
           </section>
         )}
