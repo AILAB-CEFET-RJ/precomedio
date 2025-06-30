a8c63bf456c6c50900eb71882e0fbee6d8ca88b0
Thales M <mbthales10@gmail.com>
2025-06-29 21:16:41 -0300

    Implementação da remoção de produtos notificados

--- frontend/src/components/Alerts.jsx
+++ frontend/src/components/Alerts.jsx
@@ -26,8 +26,8 @@ const Alerts = () => {
 
   const loadAlertProducts = () => {
     try {
-        const storedProducts = localStorage.getItem('idealPriceProducts');
-        if (storedProducts) {
+      const storedProducts = localStorage.getItem('idealPriceProducts');
+      if (storedProducts) {
         const parsedProducts = JSON.parse(storedProducts);
         setAlertProducts(parsedProducts);
       } else {
@@ -38,6 +38,30 @@ const Alerts = () => {
       setAlertProducts([]);
     }
   };
+
+  const removeAllAlertProducts = () => {
+    try {
+      localStorage.removeItem('idealPriceProducts');
+      setAlertProducts([]);
+    } catch (error) {
+      console.error("Erro ao excluir produtos de alerta do localStorage:", error);
+    }
+  };
+
+  const removeAlertProduct = (model) => {
+    try {
+      const storedProducts = localStorage.getItem('idealPriceProducts');
+      if (storedProducts) {
+        const parsedProducts = JSON.parse(storedProducts);
+        const updatedProducts = parsedProducts.filter(prod => prod.Model !== model);
+        console.log("Produtos atualizados:", updatedProducts);
+        localStorage.setItem('idealPriceProducts', JSON.stringify(updatedProducts));
+        setAlertProducts(updatedProducts);
+      }
+    } catch (error) {
+      console.error("Erro ao excluir produto de alerta do localStorage:", error);
+    }
+  }
 
   useEffect(() => {
     loadPriceAlerts()
@@ -65,105 +89,118 @@ const Alerts = () => {
   };
 
   return (
-      <>
-        <div id="container-fluid container2">
-          <Header />
+    <>
+      <div id="container-fluid container2">
+        <Header />
 
-          <section className="pt-5">
-            <article>
-              <div className="container">
-                <table className="table table-striped">
-                  <thead className="thead-dark">
-                    <tr >
-                        <th id="item-header-table" className="align-middle" scope="col">Id</th>
-                        <th id="item-header-table" className="align-middle" scope="col">Busca</th>
-                        <th id="item-header-table" className="align-middle" scope="col">Meta de Preço</th>
-                        <th id="item-header-table" className="align-middle" scope="col">Excluir</th>
-                        {<th id="item-header-table" className="align-middle" scope="col">Editar</th>}
-                    </tr>
-                  </thead>
+        <section className="pt-5">
+          <article>
+            <div className="container">
+              <table className="table table-striped">
+                <thead className="thead-dark">
+                  <tr >
+                    <th id="item-header-table" className="align-middle" scope="col">Id</th>
+                    <th id="item-header-table" className="align-middle" scope="col">Busca</th>
+                    <th id="item-header-table" className="align-middle" scope="col">Meta de Preço</th>
+                    <th id="item-header-table" className="align-middle" scope="col">Excluir</th>
+                    {<th id="item-header-table" className="align-middle" scope="col">Editar</th>}
+                  </tr>
+                </thead>
 
-                  <tbody>
-                    {alertPrices.length > 0 && alertPrices ? alertPrices.map((alert, i) => (
-                        <tr
-                            key={i}
+                <tbody>
+                  {alertPrices.length > 0 && alertPrices ? alertPrices.map((alert, i) => (
+                    <tr
+                      key={i}
+                    >
+                      <td>{alert.id}</td>
+                      <td>{alert.SearchString}</td>
+                      <td>{alert.target_price}</td>
+                      <td>
+                        <button
+                          onClick={() => handleDeleteAlertModal(alert)}
+                          type="button"
+                          className="btn bg-danger"
                         >
-                            <td>{alert.id}</td>
-                            <td>{alert.SearchString}</td>
-                            <td>{alert.target_price}</td>
-                            <td>
-                                <button
-                                    onClick={() => handleDeleteAlertModal(alert)}
-                                    type="button"
-                                    className="btn bg-danger"
-                                >
-                                    <FaRegTrashAlt size={20} className="bg-danger text-white" />
-                                </button>
-                            </td>
-                            {<td>
-                                <button
-                                    onClick={() => handleEditAlertModal(alert)}
-                                    type="button"
-                                    className="btn bg-success"
-                                >
-                                    <FaEdit size={20} className="bg-success text-white" />
-                                </button>
-                            </td>}
-                        </tr>
-                    )) : (
-                        <tr>
-                            <td colSpan="6" style={{ color: "#7d0000" }} className="h6 text-center ">
-                                Nenhum alerta criado
-                            </td>
-                        </tr>
-                    )}
-                  </tbody>
-                </table>
-              </div>
-            </article>
+                          <FaRegTrashAlt size={20} className="bg-danger text-white" />
+                        </button>
+                      </td>
+                      {<td>
+                        <button
+                          onClick={() => handleEditAlertModal(alert)}
+                          type="button"
+                          className="btn bg-success"
+                        >
+                          <FaEdit size={20} className="bg-success text-white" />
+                        </button>
+                      </td>}
+                    </tr>
+                  )) : (
+                    <tr>
+                      <td colSpan="6" style={{ color: "#7d0000" }} className="h6 text-center ">
+                        Nenhum alerta criado
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </article>
 
-              
-            <article id="table-home">
-              <div className="table-responsive">
-                <table className="table table-striped">
-                  <thead className="thead-dark">
-                      <tr >
-                      <th id="item-header-table" className="align-middle" scope="col">Modelo</th>
-                      <th id="item-header-table" className="align-middle" scope="col">Preço</th>
-                      <th id="item-header-table" className="align-middle" scope="col">Fornecedor</th>
-                      <th id="item-header-table" className="align-middle" scope="col">Armazenamento</th>
-                      <th id="item-header-table" className="align-middle" scope="col">Data</th>
-                      </tr>
-                  </thead>
+          <article id="table-home">
+            <div className="table-responsive">
+              <table className="table table-striped">
+                <thead className="thead-dark">
+                  <tr >
+                    <th id="item-header-table" className="align-middle" scope="col">Modelo</th>
+                    <th id="item-header-table" className="align-middle" scope="col">Preço</th>
+                    <th id="item-header-table" className="align-middle" scope="col">Fornecedor</th>
+                    <th id="item-header-table" className="align-middle" scope="col">Armazenamento</th>
+                    <th id="item-header-table" className="align-middle" scope="col">Data</th>
+                    <th id="item-header-table" className="align-middle" scope="col">Excluir</th>
+                  </tr>
+                </thead>
 
-                  <tbody>
-                    {alertProducts.length > 0 ? alertProducts.map((prod, i) => (
-                      <tr key={i}>
-                        <td>{prod.Model}</td>
-                        <td>R$ {prod.Price.replace(".", ",")}</td>
-                        <td>{prod.Supplier}</td>
-                        <td>{prod.SearchString.split("+")[1]}</td>
-                        <td>{prod.DateOfSearch.split(/((\d){2,4}-(\d){2,2}-(\d){2,2})/)[1]}</td>
-                      </tr>
-                    )) : (
-                      <tr>
-                        <td colSpan="6" style={{ color: "#7d0000" }} className="h6 text-center ">
+                <tbody>
+                  {alertProducts.length > 0 ? alertProducts.map((prod, i) => (
+                    <tr key={i}>
+                      <td>{prod.Model}</td>
+                      <td>R$ {prod.Price.replace(".", ",")}</td>
+                      <td>{prod.Supplier}</td>
+                      <td>{prod.SearchString.split("+")[1]}</td>
+                      <td>{prod.DateOfSearch.split(/((\d){2,4}-(\d){2,2}-(\d){2,2})/)[1]}</td>
+                      <td>
+                        <button
+                          onClick={() => removeAlertProduct(prod.Model)}
+                          type="button"
+                          className="btn bg-danger"
+                        >
+                          <FaRegTrashAlt size={20} className="bg-danger text-white" />
+                        </button>
+                      </td>
+                    </tr>
+                  )) : (
+                    <tr>
+                      <td colSpan="6" style={{ color: "#7d0000" }} className="h6 text-center ">
                         Nenhum dado carregado
-                        </td>
-                      </tr>
-                    )}
-                  </tbody>
-                </table>
-              </div>
-            </article>
-          </section>
-        </div>
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </article>
+
+          <button className="btn btn-danger mx-1" onClick={removeAllAlertProducts}>
+            Limpar
+          </button>
+        </section>
+      </div>
 
       <AlertModal status={showAlertModal} changeStatus={handleAlertModal} searchString={lastSearch} deleteMode={isAlertCreated} editMode={editMode} alertId={
-          alertPrices.find(alert => alert.SearchString.toLowerCase() === lastSearch.toLowerCase())?.id
+        alertPrices.find(alert => alert.SearchString.toLowerCase() === lastSearch.toLowerCase())?.id
       } currentPrice={currentPrice} updateAlerts={setAlertPrices}
-
       />
+
     </>
   );
 };
